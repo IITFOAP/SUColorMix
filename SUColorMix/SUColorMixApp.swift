2310a10c4ea804a65516b86367d66fc94d1fddbd
Baranov Roman <palamarmle@gmail.com>
2023-06-11 22:53:47 +0300

    Initial Commit

---
+++ SUColorMix/SUColorMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUColorMixApp.swift
+//  SUColorMix
+//
+//  Created by Рома Баранов on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUColorMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
